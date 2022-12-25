6740d8f40bcf3f2be608e96066063af9ba0ae888
Sinizano77 <sinizano77@gmail.com>
2022-12-24 17:38:46 -0800

    Got logic for 2 player to work

--- game_session_class.py
+++ game_session_class.py
@@ -30,6 +30,7 @@ class GameSession:
         self._next_round_players = player_list[:]
         self._curr_rounds = 1
         self._chosen_players_for_turn = list()
+        self._temp_removed_player = list() #[Player, 'current/next']
         self._total_probability = 100 #this number decreases for every option that gets pulled out of self._probabilities
     
     def get_round(self):
@@ -74,10 +75,21 @@ class GameSession:
             print(self._next_round_players)
             self._chosen_players_for_turn.append(self._current_players_left[0])
             del self._current_players_left[0]
+        elif len(self._temp_removed_player) == 2:
+            # need to put temp player back if they exist
+            if self._temp_removed_player[1] == 'current':
+                self._current_players_left.append(self._temp_removed_player[0])
+                print([player.get_name() for player in self._current_players_left])
+            else:
+                self._next_round_players.append(self._temp_removed_player[0])
+            print('temp player returned back to pool cuz randomize')
+            del self._chosen_players_for_turn[1]
+            self._temp_removed_player.clear()
 
         # get second player on standby
-        all_available_players = list(set([player.get_id() for player in self._current_players_left] + [player.get_id() for player in self._next_round_players]))
+        all_available_players = list(set([player.get_id() for player in self._current_players_left] + [player.get_id() for player in self._next_round_players if player.get_id() != self._chosen_players_for_turn[0].get_id()]))
         print('before if statement to check all available players')
+        print(all_available_players)
         if (len(all_available_players) != 0):
             print('in if statement')
             random_index = random.randint(0, len(all_available_players) - 1)
@@ -85,15 +97,21 @@ class GameSession:
             for player in self._player_list:
                 if player.get_id() == second_player_id:
                     print('found player 2')
+                    print(second_player_id)
                     self._chosen_players_for_turn.append(player)
                     break
 
     # needed second player
     def remove_player_from_pool(self) -> None:
+        print('temporariliy removing second player')
         player = self._chosen_players_for_turn[1]
+        self._temp_removed_player.append(player)
+        print(player.get_name())
         if player in self._current_players_left:
+            self._temp_removed_player.append('current')
             self._current_players_left.remove(player)
         else:
+            self._temp_removed_player.append('next')
             self._next_round_players.remove(player)
             print('next round player removed')
 
@@ -148,7 +166,7 @@ class GameSession:
     # will return the name of the game
     def generate_game(self, potential_players: list, skip_game: Minigame) -> Minigame:
         print('generating games with all players')
-        print(potential_players)
+        print([players.get_name() for players in potential_players])
         game_name = self.get_game()
 
         # if multiplayer enabled and only one player left in round, generate single player game
@@ -173,6 +191,8 @@ class GameSession:
             self.remove_player_from_pool()
         elif len(self._chosen_players_for_turn) == 2:
             del self._chosen_players_for_turn[1]
+        print('chosen player before returning to main game logic')
+        print(self._chosen_players_for_turn)
         return [game, self._chosen_players_for_turn]
     
     def determine_point_value_and_add_to_data(self, game, value):
@@ -193,6 +213,8 @@ class GameSession:
             final_value = round(final_value)
         
         self.add_points_to_player(int(final_value))
+        print('clear temp player cuz game deleted')
+        self._temp_removed_player.clear()
         self.delete_game(game)
     
     def add_points_to_player(self, point_rating):
